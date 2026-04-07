64066ccf467f824e6e287c07edde8625d2193d4f
het-codes-ai <modijignisha12@gmail.com>
2026-04-07 21:26:39 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,8 @@
+name='HET MODI'
+qualification='HSC sci PASS'
+dream_college='LDCE'
+dream_branch='AI ML'
+
+print('My name is',name)
+print('My qualification is',qualification)
+print('my dream college is',dream_college,'with a dream branch of',dream_branch)
