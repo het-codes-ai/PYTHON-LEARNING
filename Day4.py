a4c2969be58da4f75ca10a7a56242965b46daee7
het-codes-ai <modijignisha12@gmail.com>
2026-04-07 22:39:17 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,19 @@
+age1=19
+age2=18
+age3=17
+
+if age1>=18:
+	print('man1 is adult')
+ 
+else:
+  	print('man1 is minor')
+		
+if age2>=18:
+	print('man2 is adult')
+else:
+	print('man2 is minor')
+		
+if age3>=18:
+	print('man3 is adult')
+else:
+	print('man3 is minor')
