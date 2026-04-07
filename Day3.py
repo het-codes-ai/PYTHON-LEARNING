5ad44d201fafff03dd059ff0c878c66d9cc7a866
het-codes-ai <modijignisha12@gmail.com>
2026-04-07 22:13:20 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,27 @@
+p=94
+c=95
+m=85
+e=88
+cs=99
+pp=48
+cp=47
+csp=43
+
+total1=p+c+m
+print('science marks is',total1)
+
+total2=p+c+ m+e+cs
+print('total theory marks is',total2)
+
+total3=p+c+m+e+cs+pp+cp+csp
+print('overall total is',total3)
+
+spr=total1/3
+print('science percentage=',spr)
+ 
+tpr=total2/5
+print('theory percentage=',tpr)
+
+opr=total3/6.5
+print('overall percentage=',opr)
+
