1ac22446c57a5dab1dc67394a467fe7bd608fd23
het-codes-ai <modijignisha12@gmail.com>
2026-04-08 21:22:45 +0530

    Create Day5.py

---
+++ Day5.py
@@ -0,0 +1,8 @@
+name=input("enter your name")
+print('My name is ::',name)
+
+age = int(input("what is your age?"))
+print('I am ',age,' years old')
+
+pr=float(input("what is your hsc percentage"))
+print('I scored ', pr , 'percentage in my 12th science exam')
